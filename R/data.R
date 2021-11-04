6f9964dc1c69f0288ae68df940f660eb59748ef4
missuse <mdragicevic@ibiss.bg.ac.rs>
2021-11-04 12:18:01 +0100

    first comit

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Background data for Ramachandran diagram
+#'
+#' A list with four elements "general", "glycine", "pre.pro" and "proline".
+#' Each element is a matrix containing the background data used by
+#' \code{\link{ggplot2}{geom_tile}} to show favorable regions of torsion angles
+#'
+#' @references Lovell, S.C., Davis, I.W., Arendall, W.B., III, de Bakker, P.I.W., Word, J.M., Prisant, M.G., Richardson, J.S. and Richardson, D.C. (2003), Structure validation by C\eqn{\alpha} geometry: \eqn{\phi},\eqn{\psi} and C\eqn{\beta} deviation. Proteins, 50: 437-450. https://doi.org/10.1002/prot.10286
+"background_dist"
