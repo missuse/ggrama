751df63c7442dea1be09972ef7c55ae675e829e3
missuse <mdragicevic@ibiss.bg.ac.rs>
2021-11-09 20:15:29 +0100

    akima bicubic add

--- R/internal.R
+++ R/internal.R
@@ -11,19 +11,3 @@ areColors <- function(x) {
              error = function(e) FALSE)
   })
 }
-
-#'  1D linear matrix resize
-#'
-#' @param img input matrix
-#' @param new_width output matrix width
-#' @param new_height output matrix height
-#'
-#' @return  resized matrix of dimensions new_height * new_width
-#' @import stats
-
-resize_mat_linear <- function(img, new_width, new_height) {
-  new_img <- apply(img, 2, function(y) stats::approx(y, n = new_height)$y)
-  new_img <- t(apply(new_img, 1, function(y) stats::approx(y, n = new_width)$y))
-  new_img[new_img < 0] <- 0
-  return(new_img)
-}
